b4bc0772d787ba8fc9197fa3b00b1faec5d48954
ivo2291 <ivo2291@gmail.com>
2023-10-27 14:54:34 +0300

    Added more tasks

--- DataOperationsInDjangoExercise/caller.py
+++ DataOperationsInDjangoExercise/caller.py
@@ -1,11 +1,13 @@
 import os
+from datetime import datetime
+
 import django
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-from main_app.models import Pet, Artifact, Location
+from main_app.models import Pet, Artifact, Location, Car, Task
 
 
 # 01. Pet task
@@ -44,7 +46,7 @@ def delete_all_artifacts():
 #         region=region,
 #         population=population,
 #         description=description,
-#         is_capital=is_capital,
+#         is_capital=is_capital
 #     )
 
 
@@ -60,10 +62,10 @@ def show_all_locations():
 
 
 def new_capital():
+    # Location.objects.filter(pk=1).update(is_capital=True) --> Faster solution
+
     location = Location.objects.first()
-
     location.is_capital = True
-
     location.save()
 
 
@@ -79,3 +81,65 @@ def delete_first_location():
 
 # 04. Car task
 
+# def add_car(model: str, year: int, color: str, price: float):
+#     Car.objects.create(
+#         model=model,
+#         year=year,
+#         color=color,
+#         price=price
+#     )
+def apply_discount():
+    cars = Car.objects.all()
+
+    for car in cars:
+        percentage_of_discount = sum(int(digit) for digit in str(car.year)) / 100
+        car_price = float(car.price)
+        car.price_with_discount = car_price - car_price * percentage_of_discount
+        car.save()
+
+
+def get_recent_cars():
+    recent_cars = Car.objects.all().filter(year__gte=2020).values('model', 'price_with_discount')
+
+    return recent_cars
+
+
+def delete_last_car():
+    Car.objects.last().delete()
+
+
+# 05. Task task
+
+def add_new_task(title: str, description: str, due_date: str):
+    Task.objects.create(
+        title=title,
+        description=description,
+        due_date=due_date
+    )
+
+
+def show_unfinished_tasks():
+    # tasks_list = []
+
+    unfinished_tasks = Task.objects.all().filter(is_finished=False)
+
+    # this is without __str__ method in the model:
+    # for task in unfinished_tasks:
+    #     tasks_list.append(f"Task - {task.title} needs to be done until {task.due_date}!")
+
+    return '\n'.join(str(t) for t in unfinished_tasks)
+
+
+def complete_odd_tasks():
+    tasks = Task.objects.all()
+
+    for task in tasks:
+        if task.id % 2 != 0 and not task.is_finished:
+            task.is_finished = True
+            task.save()
+
+
+def encode_and_replace(text: str, task_title: str):
+    encoded_text = ''.join([chr(ord(char) - 3) for char in text])
+
+    Task.objects.all().filter(title=task_title).update(description=encoded_text)
