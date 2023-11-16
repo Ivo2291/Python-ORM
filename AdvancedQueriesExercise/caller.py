830d943c105f88e44f30affd5ddbdb52781e09f3
ivo2291 <ivo2291@gmail.com>
2023-11-16 14:06:11 +0200

    Added more exercises

--- AdvancedQueriesExercise/caller.py
+++ AdvancedQueriesExercise/caller.py
@@ -7,7 +7,8 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-from main_app.models import RealEstateListing, VideoGame, BillingInfo, Invoice, Technology, Project, Programmer, Task
+from main_app.models import RealEstateListing, VideoGame, BillingInfo, Invoice, Technology, Project, Programmer, Task, \
+    Exercise
 
 # 01. Real Estate Listing task with custom manager -> Test code:
 # -------------------------------------------------------------
@@ -213,10 +214,55 @@ from main_app.models import RealEstateListing, VideoGame, BillingInfo, Invoice, 
 #     print('- ' + task.title)
 
 # 4. Get recent completed tasks
-recent_completed = task1.recent_completed_tasks(days=5)
-print("Recent Completed Tasks:")
-for task in recent_completed:
-    print('- ' + task.title)
+# recent_completed = task1.recent_completed_tasks(days=5)
+# print("Recent Completed Tasks:")
+# for task in recent_completed:
+#     print('- ' + task.title)
 
 # 06. Gym Session task -> Test code:
 # ---------------------------------
+# Create instances of Exercise
+# exercise1 = Exercise.objects.create(
+#     name="Push-ups",
+#     category="Strength",
+#     difficulty_level=4,
+#     duration_minutes=10,
+#     repetitions=50,
+# )
+#
+# exercise2 = Exercise.objects.create(
+#     name="Running",
+#     category="Cardio",
+#     difficulty_level=7,
+#     duration_minutes=20,
+#     repetitions=0,
+# )
+#
+# exercise3 = Exercise.objects.create(
+#     name="Pull-ups",
+#     category="Strength",
+#     difficulty_level=13,
+#     duration_minutes=35,
+#     repetitions=20,
+# )
+
+# Print the results
+# long_and_hard_exercises = Exercise.get_long_and_hard_exercises()
+# print("Long and hard exercises:")
+# for exercise in long_and_hard_exercises:
+#     print('- ' + exercise.name)
+#
+# short_and_easy_exercises = Exercise.get_short_and_easy_exercises()
+# print("Short and easy exercises:")
+# for exercise in short_and_easy_exercises:
+#     print('- ' + exercise.name)
+#
+# exercises_within_duration = Exercise.get_exercises_within_duration(20, 40)
+# print(f"Exercises within 20 - 40 minutes:")
+# for exercise in exercises_within_duration:
+#     print('- ' + exercise.name)
+#
+# exercises_with_difficulty_and_repetitions = Exercise.get_exercises_with_difficulty_and_repetitions(6, 15)
+# print(f"Exercises with difficulty 6+ and repetitions 15+:")
+# for exercise in exercises_with_difficulty_and_repetitions:
+#     print('- ' + exercise.name)
--- AdvancedQueriesExercise/main_app/models.py
+++ AdvancedQueriesExercise/main_app/models.py
@@ -192,8 +192,38 @@ class Task(models.Model):
 
 
 class Exercise(models.Model):
-    name = models.CharField(max_length=100)
-    category = models.CharField(max_length=50)
+    name = models.CharField(
+        max_length=100,
+    )
+
+    category = models.CharField(
+        max_length=50,
+    )
+
     difficulty_level = models.PositiveIntegerField()
+
     duration_minutes = models.PositiveIntegerField()
+
     repetitions = models.PositiveIntegerField()
+
+    @classmethod
+    def get_long_and_hard_exercises(cls):
+        query = Q(duration_minutes__gt=30) & Q(difficulty_level__gte=10)
+
+        return cls.objects.filter(query)
+
+    @classmethod
+    def get_short_and_easy_exercises(cls):
+        query = Q(duration_minutes__lt=15) & Q(difficulty_level__lt=5)
+
+        return cls.objects.filter(query)
+
+    @classmethod
+    def get_exercises_within_duration(cls, min_duration: int, max_duration: int):
+        return cls.objects.filter(duration_minutes__range=(min_duration, max_duration))
+
+    @classmethod
+    def get_exercises_with_difficulty_and_repetitions(cls, min_difficulty: int, min_repetitions: int):
+        query = Q(difficulty_level__gte=min_difficulty) & Q(repetitions__gte=min_repetitions)
+
+        return cls.objects.filter(query)
